2f6a10ff6909203321442c4dd98e216af2cad1f2
cwf <cwfflutter@gmail.com>
2021-07-23 00:31:32 +0800

    start learning part 20

--- code/19-raycaster/src/script.js
+++ code/19-raycaster/src/script.js
@@ -45,10 +45,10 @@ scene.add(object1, object2, object3)
  const rayOrigin = new THREE.Vector3(- 3, 0, 0)
  const rayDirection = new THREE.Vector3(10, 0, 0)
  rayDirection.normalize()
-raycaster.set(rayOrigin, rayDirection)
 
-const intersect  = raycaster.intersectObjects([object1, object2, object3])
-console.log(intersect)
+
+// const intersect  = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersect)
 /**
  * Sizes
  */
@@ -71,7 +71,37 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+/**
+ * Mouse
+ */
+ const mouse = new THREE.Vector2()
 
+ window.addEventListener('mousemove', (event) =>
+ {
+     mouse.x = event.clientX / sizes.width * 2 - 1
+     mouse.y = - (event.clientY / sizes.height) * 2 + 1
+ })
+
+ window.addEventListener('click', () =>
+{
+    if(currentIntersect)
+    {
+        switch(currentIntersect.object)
+        {
+            case object1:
+                console.log('click on object 1')
+                break
+
+            case object2:
+                console.log('click on object 2')
+                break
+
+            case object3:
+                console.log('click on object 3')
+                break
+        }
+    }
+})
 /**
  * Camera
  */
@@ -101,7 +131,54 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
+    // Cast a fixed ray
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
+    
+    // raycaster.set(rayOrigin, rayDirection)
+    //  const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
+    // for(const object of objectsToTest)
+    // {
+    //     object.material.color.set('#ff0000')
+    // }
+
+    // for(const intersect of intersects)
+    // {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
+
+      // Cast a ray from the mouse and handle events
+      raycaster.setFromCamera(mouse, camera)
+
+      const objectsToTest = [object1, object2, object3]
+      const intersects = raycaster.intersectObjects(objectsToTest)
+      
+      if(intersects.length)
+      {
+          if(!currentIntersect)
+          {
+              console.log('mouse enter')
+          }
+  
+          currentIntersect = intersects[0]
+      }
+      else
+      {
+          if(currentIntersect)
+          {
+              console.log('mouse leave')
+          }
+          
+          currentIntersect = null
+      }
     // Update controls
     controls.update()
 
